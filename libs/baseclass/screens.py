fe89cfa84dd61e06621e21574a844250a8a1ecac
Naren Rawat <86599661+NarenRawat@users.noreply.github.com>
2021-08-13 11:44:08 +0530

    Create screens for MainCard
    Following screens are created:
        - MusicScreen
        - PlaylistsScreen
        - FavoritesScreen
        - AboutScreen
        - SettingsScreen

---
+++ libs/baseclass/screens.py
@@ -0,0 +1,26 @@
+from kivymd.uix.screen import MDScreen
+
+
+class MusicScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(MusicScreen, self).__init__(**kwargs)
+
+
+class PlaylistsScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(PlaylistsScreen, self).__init__(**kwargs)
+
+
+class FavoritesScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(FavoritesScreen, self).__init__(**kwargs)
+
+
+class AboutScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(AboutScreen, self).__init__(**kwargs)
+
+
+class SettingsScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(SettingsScreen, self).__init__(**kwargs)
