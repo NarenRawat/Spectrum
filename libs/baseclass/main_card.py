5c6a6f41e4bd1ef4398fc79a9c39038860e90764
Naren Rawat <86599661+NarenRawat@users.noreply.github.com>
2021-08-17 15:34:19 +0530

    Refactor codes

--- libs/baseclass/main_card.py
+++ libs/baseclass/main_card.py
@@ -6,26 +6,21 @@ from kivymd.uix.card import MDCard
 
 class MainCard(MDCard):
     closed = BooleanProperty(True)
-    anim_duration = 0.1
 
     def __init__(self, **kwargs):
         super(MainCard, self).__init__(**kwargs)
 
     def toggle(self):
         if self.closed:
-            anim = Animation(
+            Animation(
                 size_hint_y=0.9,
                 x=self.parent.ids.menu.right + 50,
                 center_y=Window.height/2,
-                duration=self.anim_duration
-            )
-            anim.start(self)
+                duration=0.1).start(self)
             self.closed = False
         else:
-            anim = Animation(
+            Animation(
                 size_hint=(1, 1),
                 pos=(0, 0),
-                duration=self.anim_duration
-            )
-            anim.start(self)
+                duration=0.1).start(self)
             self.closed = True
